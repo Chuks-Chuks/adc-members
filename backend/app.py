f96ce4800eb62fbb4aef672995bb0d6b882c8b3d
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-07-03 13:40:11 +0100

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('C:/Users/phili/adc-forms')
+from flask import Flask, request
+from db import get_connection
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Welcome!<h1>'
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO adc_members (membership_id, first_name, last_name, email, address, phone_number, local_government, polling_unit)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        data['membership_id'],
+        data['first_name'],
+        data['last_name'],
+        data['email'],
+        data['address'],
+        data['phone_number'],
+        data['local_government'],
+        data['polling_unit']
+    ))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Submission successful!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
